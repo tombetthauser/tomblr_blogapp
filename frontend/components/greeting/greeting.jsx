5fa0610266da7d07caa9beb8939710ad11440adc
tombetthauser <tombetthauser@gmail.com>
2020-01-30 13:10:03 -0800

    Tweaks welcome message

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -11,7 +11,7 @@ const Greeting = ({ currentUser, logout }) => {
   );
   const personalGreeting = () => (
     <hgroup className="header-group">
-      <h2 className="header-name">wecome back {currentUser.username}</h2>
+      <h2 className="header-name">whlelclolmele blackle {currentUser.username}lele</h2>
       <button className="header-button" onClick={logout}>logout</button>
     </hgroup>
   );
