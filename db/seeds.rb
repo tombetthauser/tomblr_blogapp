d2185aa429c3b5b267be5aa4b8c81a205775a4e8
tombetthauser <tombetthauser@gmail.com>
2020-03-20 14:31:39 -0700

    Old update of seeds before new work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# some seed wrestler image urls from tom -- to use later when making seeds
+
+# Chyna
+# https://thenypost.files.wordpress.com/2019/08/chyna_getty.gif
+
+# Charlotte Flair
+# https://sportzwiki.com/wp-content/uploads/2017/04/charlotte-flair-seth-rollins-wwe-fittest-50.jpg
+# https://i.pinimg.com/originals/3b/fe/a6/3bfea6f3e87991aec59d37fee0ec31b3.jpg
+
+# The Rock
+# https://upload.wikimedia.org/wikipedia/commons/f/f1/Dwayne_Johnson_2%2C_2013.jpg
+
+# Steve Austin
+# https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Austin_by_Gage_Skidmore.jpg
