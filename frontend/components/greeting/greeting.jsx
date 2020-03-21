48e999e0f3bd873717c1a897ba9c2a8ecbbae0dc
tombetthauser <tombetthauser@gmail.com>
2020-03-20 20:46:19 -0700

    Clean up greeting.jsx

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const GREETINGS = [
-  ["welcome back ", ""],
-  // ["hello ", ", glad to see you're back"],
-  // ["nice to see you again ", ""],
-  // ["always a pleasure ", ""],
-  // ["howdy ", "!"],
-  // ["low-key, I'm so happy to see you ", ""],
-  // ["pog to see you again ", ""],
-  // ["hi there jason (just kidding ", ")"],
-  // ["hello again ", "sy"],
-  // ["hows it goin ", "?"]
-]
+const GREETINGS = [["welcome back ", ""]];
 
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <div>
       <header>
         <Link to="/" className="header-link">
-          <h1>hello</h1>
+          <h1>tomblr</h1>
         </Link>
       </header>
       <nav className="login-signup">
-        {/* <Link to="/" className="header-link"> */}
-          <p>Come for what you love.<br />Stay for what you discover.</p>
-        {/* </Link> */}
+        <p>Come for what you love.<br/>Stay for what you discover.</p>
         <Link to="/signup">Get Started</Link>
         <Link to="/login">Log In</Link>
       </nav>
