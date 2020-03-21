4ae61817fc4a45ddcf4ef35fc50f6985077c7c49
tombetthauser <tombetthauser@gmail.com>
2020-03-20 21:38:12 -0700

    Get DOM content selection success

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -1,1 +1,7 @@
-alert("hello there!")
+document.addEventListener("DOMContentLoaded", (event) => {
+  setTimeout(() => {
+    // alert("yo yo yo")
+    alert(document.querySelector(".background-animation-div-1"))
+  }, 3000)
+  // alert(document.querySelector(".background-animation-div-1"))
+})
