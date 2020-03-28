812a340e487d2f158413d4f43ceab3ce31002c11
tombetthauser <tombetthauser@gmail.com>
2020-03-28 00:38:32 -0700

    Make new blog page container

---
+++ frontend/components/new_blog_page/new_blog_page_container.js
@@ -0,0 +1,21 @@
+import { fetchBlog, fetchBlogs, deleteBlog } from '../../actions/blog_actions';
+import { requestUser } from '../../actions/user_actions';
+import { selectBlog } from '../../reducers/selectors';
+import { connect } from 'react-redux';
+import NewBlogPage from './new';
+import { createBlog, deleteBlog } from '../../actions/blog_actions';
+
+const mapStateToProps = (state, { match }) => ({
+  // blog: selectBlog(state, match.params.blogId),
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  createBlog: blog => dispatch(createBlog(blog)),
+  // fetchBlogs: () => dispatch(fetchBlogs()),
+  // requestUser: userId => dispatch(requestUser(userId)),
+  // deleteBlog: blogId => dispatch(deleteBlog(blogId)),
+  // fetchBlog: id => dispatch(fetchBlog(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewBlogPage);
